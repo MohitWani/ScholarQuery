6fdd9fcbc871bd8e2fb7f1d852392e76cff5891b
MohitWani <wanimohit1@gmail.com>
2024-09-10 13:17:25 +0530

    test

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.embeddings import OllamaEmbeddings
+from langchain.vectorstores import FAISS, Chroma
+
+from fastapi import FastAPI
+
+app = FastAPI()
