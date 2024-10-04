8d1f071807b8478f7619b7ec3172fd41ea0ec712
MohitWani <wanimohit1@gmail.com>
2024-10-04 17:45:48 +0530

    GENERATION IS DONE

--- Generation.py
+++ Generation.py
@@ -2,23 +2,38 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-from Retrieval import retrieval
-template = """Answer the question of user by using the context:
+from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain_community.vectorstores import FAISS
 
-            {context}
 
-            Question: {question}
-            """
-llm = ChatGroq(groq_api_key="",
-                model_name="mixtral-8x7b-32768"
+def get_relavant_doc(query, persist_directory, ):
+    
+    relavent_docs = retrieval.invoke(query)
+    return relavent_docs
+
+
+def get_output(retrieval):
+    template = """Answer the question of user by using the context:
+
+                {context}
+
+                Question: {question}
+                """
+    llm = ChatGroq(
+                    groq_api_key="",
+                    model_name="mixtral-8x7b-32768"
                 )
 
-prompt = ChatPromptTemplate(template)
+    prompt = ChatPromptTemplate(template)
 
-chain = (
-            {"context": retrieval, "question":RunnablePassthrough()}
-            | prompt
-            | llm
-            | StrOutputParser
-        )
+    chain = (
+                {"context": retrieval | get_relavant_doc, "question":RunnablePassthrough()}
+                | prompt
+                | llm
+                | StrOutputParser
+            )
+    return chain
 
+if __name__=="__main__":
+    embedding = GPT4AllEmbeddings()
+    retrieval = FAISS.load_local('persist_directory', 'embedding')
