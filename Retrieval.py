c62cc5fbca4e3b6343670a8196b916675f063646
MohitWani <wanimohit1@gmail.com>
2024-10-04 16:42:58 +0530

    Retrieval update

--- Retrieval.py
+++ Retrieval.py
@@ -3,22 +3,37 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.indexes import VectorstoreIndexCreator
 from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain_community.vectorstores import FAISS
+import os
 
 
-document = PyPDFLoader("document.pdf").load()
+def load_document(file_path):
+    document = PyPDFLoader(file_path).load()
+    return document
 
-doc_split = RecursiveCharacterTextSplitter(
-    chunk_size=100,
-    chunk_overlape = 200,
-)
+def splitter(document):
+    doc_split = RecursiveCharacterTextSplitter(
+        chunk_size=100,
+        chunk_overlape = 200,
+    )
 
-docs = doc_split.split_documents(documents=document)
+    docs_metadata = doc_split.split_documents(documents=document)
 
-docs = [doc for doc in docs]
+    docs = [doc for doc in docs]
+    return docs
 
-embedding = GPT4AllEmbeddings()
 
-persist_db = "D:\my_projects\ScholarQuery"
-vectorstore = FAISS.from_documents(docs, embedding, persist_directory=persist_db)
+def create_vectorstore(docs, embedding, path_tosave):
 
-vectorstore.save_local(persist_db)
+    vectorstore = FAISS.from_texts(docs, embedding)
+
+    path = path_tosave
+    folder = "faiss_index"
+
+    persist_db = os.path.join(path, folder)
+    os.makedirs(persist_db, exist_ok=True)
+
+    vectorstore.save_local(persist_db)
+    return "Vector database is Saved."
+
+if __name__=="__main__":
+    embedding = GPT4AllEmbeddings()
